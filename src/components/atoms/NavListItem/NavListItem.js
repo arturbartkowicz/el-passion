6f39d7821cf626bc39427cb7f356a30bdcd9ff78
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-11-28 10:50:43 +0100

    added icons

--- src/components/atoms/NavListItem/NavListItem.js
+++ src/components/atoms/NavListItem/NavListItem.js
@@ -1,11 +1,27 @@
 import { ListItem } from "./NavListItem.styles";
 import openIssue from "../../../assets/icons/icon-open-issue.svg";
+import closedIssue from "../../../assets/icons/icon-closed-issue.svg";
+import github from "../../../assets/icons/icon-github.svg";
 
-function NavListItem({ text, icon }) {
+function NavListItem({ text, icon, counter }) {
+  const iconPath = () => {
+    switch (icon) {
+      case "github":
+        return github;
+      case "open-issue":
+        return openIssue;
+      case "closed-issue":
+        return closedIssue;
+      default:
+        console.log("No image");
+    }
+  };
+
   return (
     <ListItem>
-      <img src={openIssue} alt="Girl in a jacket" width="20" height="20" />
+      <img src={iconPath()} alt="Girl in a jacket" width="20" height="20" />
       {text}
+      {counter}
     </ListItem>
   );
 }
--- src/components/molecules/NavigationList/NavigationList.js
+++ src/components/molecules/NavigationList/NavigationList.js
@@ -4,9 +4,9 @@ import NavListItem from "../../atoms/NavListItem/NavListItem";
 function NavigationList() {
   return (
     <List>
-      <NavListItem icon="github" text="All" />
-      <NavListItem icon="open-issue" text="Open" />
-      <NavListItem icon="closed-issue" text="Closed" />
+      <NavListItem icon="github" text="All" counter="12" />
+      <NavListItem icon="open-issue" text="Open" counter="4" />
+      <NavListItem icon="closed-issue" text="Closed" counter="8" />
     </List>
   );
 }
